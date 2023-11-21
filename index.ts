085d9b777b3edbd5521455b4f184471931abbc72
Chase Pierce <syntaqx@gmail.com>
2023-11-21 14:51:11 -0700

    dynamic good records

--- index.ts
+++ index.ts
@@ -12,10 +12,50 @@ const primaryZone = cloudflare.getZone({
     name: zoneName,
 });
 
+const githubPagesARecords = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153",
+];
+
+const githubPagesAAAARecords = [
+    "2606:50c0:8000::153",
+    "2606:50c0:8001::153",
+    "2606:50c0:8002::153",
+    "2606:50c0:8003::153",
+];
+
+const recordList = [
+    {
+        name: "docs",
+        value: "octostack.github.io",
+        type: "CNAME",
+    }
+];
+
+githubPagesARecords.forEach((value) => {
+    recordList.push({
+        name: "@",
+        value: value,
+        type: "A",
+    });
+});
+
+githubPagesAAAARecords.forEach((value) => {
+    recordList.push({
+        name: "@",
+        value: value,
+        type: "AAAA",
+    });
+});
+
 // Set up an A record within the DNS Zone
-const primaryRecord = new cloudflare.Record("primaryRecord", {
-    zoneId: primaryZone.then(primaryZone => primaryZone.id),
-    name: "test",
-    value: "192.168.0.11",
-    type: "A",
+recordList.forEach((record, i) => {
+    const _ = new cloudflare.Record(record.name + i, {
+        zoneId: primaryZone.then(primaryZone => primaryZone.id),
+        name: record.name,
+        value: record.value,
+        type: record.type,
+    });
 });
